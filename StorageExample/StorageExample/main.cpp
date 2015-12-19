420fb2328bf66950f66fcd542fba7717016fc75b
TheCoreMan <dude500@gmail.com>
2015-12-19 19:59:38 +0200

    Added basic reading from file.

--- StorageExample/StorageExample/main.cpp
+++ StorageExample/StorageExample/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
+
+typedef std::vector<char> buffer;
+
+buffer getFileData(const std::wstring& path) {
+	std::streampos size;
+
+	std::ifstream file(path, std::ios::in | std::ios::binary | std::ios::ate);
+	if (file.is_open())
+	{
+		size = file.tellg();
+		auto buf = buffer(size.operator+(1), '\0');
+		
+
+		file.seekg(0, std::ios::beg);
+		file.read(buf.data(), size);
+		file.close();
+		return buf;
+	}
+	throw std::exception("Unable to open file.");
+}
+
+void store(const buffer& data) {
+	std::cout << data.data() << std::endl;
+}
 
 int main() {
-	std::cout << "This is a line of text." << std::endl;
+	auto dataToStore = getFileData(L"C:\\important.txt");
+	store(dataToStore);
+
 	return 0;
 }
